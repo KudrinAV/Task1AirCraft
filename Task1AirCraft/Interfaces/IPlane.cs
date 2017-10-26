2548c81a43eb835e5a6f010f516398417f03700d
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-26 20:34:01 +0300

    deleted field, added struct PlaneProperties instead

--- Task1AirCraft/Interfaces/IPlane.cs
+++ Task1AirCraft/Interfaces/IPlane.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task1AirCraft.Classes.Plane;
 
 namespace Task1AirCraft.Interfaces
 {
-    public interface IPlane : IAircraft
+    public interface IPlane 
     {
-        int NumberOfEngines { get; }
+        PlaneProperties planeProperties { get; set; }
     }
 }
