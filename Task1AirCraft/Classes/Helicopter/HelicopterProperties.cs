50483e87e2b157b5f528499fa67f84737c794daa
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-26 20:48:03 +0300

    Added struct HelicopterProperties. Interface IHelicopter was changed by deleting fields and adding struct.

---
+++ Task1AirCraft/Classes/Helicopter/HelicopterProperties.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1AirCraft.Classes.Helicopter
+{
+    public struct HelicopterProperties
+    {
+        int MinimumHeightOfWork;
+        int MaximumHeightOfwork;
+        int NumberOfScrews;
+
+        public HelicopterProperties(int maximumHeightOfWork, int minimumHeightOfWork, int nymberOfScrews)
+        {
+            MinimumHeightOfWork = minimumHeightOfWork;
+            MaximumHeightOfwork = maximumHeightOfWork;
+            NumberOfScrews = nymberOfScrews;
+        }
+
+    }
+}
--- Task1AirCraft/Interfaces/IHelicopter.cs
+++ Task1AirCraft/Interfaces/IHelicopter.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task1AirCraft.Classes.Helicopter;
 
 namespace Task1AirCraft.Interfaces
 {
     public interface IHelicopter : IAircraft
     {
-        int MinimumHeightOfWork { get; }
+        HelicopterProperties helicopterProperties { get; set; }
     }
 }
