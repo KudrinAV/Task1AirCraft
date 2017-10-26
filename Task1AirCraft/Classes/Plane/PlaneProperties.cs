58359ac8775e1feb063f9fe904bf30ee452a5a23
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-26 20:32:53 +0300

    Added struct PlaneProperties. Reworked interfaces IPlane(removed 1 field, added struct instead)

---
+++ Task1AirCraft/Classes/Plane/PlaneProperties.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1AirCraft.Classes.Plane
+{
+    public struct PlaneProperties
+    {
+        string TypeOfEngine;
+        int NumberOfEngines;
+        int NumberOfPilots;
+        int NumberOfWings;
+
+        public PlaneProperties(string typeOfEngine, int numberOfEngines, int numberOfPilots, int numberOfWings)
+        {
+            TypeOfEngine = typeOfEngine;
+            NumberOfEngines = numberOfEngines;
+            NumberOfPilots = numberOfPilots;
+            NumberOfWings = numberOfWings;
+        }
+    }
+}
---
+++ task1aircraft/interfaces/iplane.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task1AirCraft.Classes.Plane;
+
+namespace Task1AirCraft.Interfaces
+{
+    public interface IPlane 
+    {
+        PlaneProperties planeProperties { get; set; }
+    }
+}
