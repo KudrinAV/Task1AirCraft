601b77eb7adc7e8f489277125430a1192efbbbff
Антон Кудрин <antonkudrin25@gmail.com>
2017-10-29 22:20:01 +0300

    added demonstration of the task to project.

--- Task1AirCraft/Program.cs
+++ Task1AirCraft/Program.cs
@@ -12,60 +12,86 @@ namespace Task1AirCraft
 {
     class Program
     {
-        static void Main(string[] args)
+        public static ICollection<IAircraft> GetListAircraft()
         {
             AircraftProperties aircraftProperties = new AircraftProperties(capacity: 200, carryingCapacity: 500, rangeOfFlight: 5000);
+            AircraftProperties aircraftProperties1 = new AircraftProperties(capacity: 200, carryingCapacity: 500, rangeOfFlight: 4000);
+            AircraftProperties aircraftProperties2 = new AircraftProperties(capacity: 200, carryingCapacity: 500, rangeOfFlight: 3000);
             PlaneProperties planeProperties = new PlaneProperties(numberOfEngines: 2, numberOfPilots: 2, accelerationLine: 500);
             HelicopterProperties helicopterProperties = new HelicopterProperties(maximumHeightOfWork: 300, minimumHeightOfWork: 50, numberOfScrews: 4);
 
-            List<IAircraft> myAirport = new List<IAircraft>() {
-                new Aircraft("boing1", "Original", aircraftProperties),
+            List<IAircraft> airport = new List<IAircraft>() {
+                new Aircraft("Original", "Original", aircraftProperties),
                 new Plane(typeOfPlane: "boing1", nameOfPlane: "Alice", aircraftProperties_: aircraftProperties, planeProperties_: planeProperties),
-                new PassengerPlane(typeOfPlane: "boing2", nameOfPlane: "Orianna", aircraftProperties_: aircraftProperties, planeProperties_: planeProperties, numberOfPlaces: 120),
-                new TransportPlane(typeOfPlane: "boing2", nameOfPlane: "TwoBe", aircraftProperties_: aircraftProperties, planeProperties_: planeProperties, power: 20),
-                new Helicopter(typeOfPlane: "Ansat1", nameOfPlane: "NineS", aircraftProperties_: aircraftProperties, helicopterProperties_: helicopterProperties),
+                new PassengerPlane(typeOfPlane: "boing2", nameOfPlane: "Orianna", aircraftProperties_: aircraftProperties1, planeProperties_: planeProperties, numberOfPlaces: 120),
+                new TransportPlane(typeOfPlane: "boing2", nameOfPlane: "TwoBe", aircraftProperties_: aircraftProperties2, planeProperties_: planeProperties, power: 20),
+                new Helicopter(typeOfPlane: "Ansat1", nameOfPlane: "NineS", aircraftProperties_: aircraftProperties1, helicopterProperties_: helicopterProperties),
                 new PassengerHelicopter(typeOfPlane: "Ansat2", nameOfPlane: "ATwo", aircraftProperties_: aircraftProperties, helicopterProperties_: helicopterProperties, numberOfPlaces: 30),
-                new TransportHelicopter(typeOfPlane: "Ansat3", nameOfPlane: "Nier", aircraftProperties_: aircraftProperties, helicopterProperties_: helicopterProperties, power: 5)
+                new TransportHelicopter(typeOfPlane: "Ansat3", nameOfPlane: "Nier", aircraftProperties_: aircraftProperties2, helicopterProperties_: helicopterProperties, power: 5)
             };
-            List<IAircraft> myAirport2 = new List<IAircraft>();
+            return airport;
+        }
 
-            //myAirport.Add(plane1);
-            //myAirport.Add(plane2);
-            //myAirport.Add(plane3);
-            //myAirport.Add(plane4);
-            //myAirport.Add(plane5);
-            //myAirport.Add(plane6);
-
-            Airport airport = new Airport(myAirport);
-
-            foreach (IAircraft item in airport.GetFindAircraftByConsumptionOfFuel(30000, 30000000))
+        public static void ShowAllAircraft(Airport airport)
+        {
+            Console.WriteLine("Все экземпляры.");
+            Console.WriteLine("|{0,-10}|{1,-20}|{2,-20}|{3,-20}|{4,20}|", "Название", "Дистанция полета", "Переносимый груз","Вместимость груза","Потребление горючего");
+            foreach (var item in airport)
             {
-                Console.WriteLine(item.NameOfPlane + " " + item.AircraftProperties_.RangeOfFlight + " " + item.AircraftProperties_.CarryingCapacity + " " + item.GetConsumptionOfFuel());
+                Console.WriteLine("|{0,-10}|{1,-20}|{2,-20}|{3,-20}|{4,-20}|", item.NameOfPlane, item.AircraftProperties_.RangeOfFlight, item.AircraftProperties_.CarryingCapacity, item.AircraftProperties_.Capacity, item.GetConsumptionOfFuel());
             }
+        }
 
-            //double TotalCaryringCapacity = 0.0;
-            //foreach(var item in airport.GetTotalCarryingCapacity())
-            //{
-            //    TotalCaryringCapacity += item;
-            //}
-            //Console.WriteLine(TotalCaryringCapacity);
+        public static void ShowGetAllMethods(Airport airport)
+        {
+            Console.WriteLine("Результаты методов полного подсчета.");
+            Console.WriteLine("Общая вместимость груза: " + airport.GetTotalCarryingCapacity());
+            Console.WriteLine("Общая вместимость : " + airport.GetTotatlCapacity());
+        }
 
-            //double TotalCapacity = 0.0;
-            //foreach(var item in airport.GetTotatlCapacity())
-            //{
-            //    TotalCapacity += item;
-            //    Console.WriteLine(item);
-            //}
+        public static void ShowGetFindByConmsumptionOfFuel(Airport airport)
+        {
+            Console.WriteLine("Демонстрация фильтрации по параметрам горючего.");
+            Console.WriteLine("Мин значение потребляемого горючего: 35000");
+            Console.WriteLine("Макс значение потребляемого горючего: 1000000");
+            Console.WriteLine("|{0,-10}|{1,-20}|", "Название", "Потребление горючего");
+            foreach (var item in airport.GetFindAircraftByConsumptionOfFuel(35000, 1000000))
+            {
+                Console.WriteLine("|{0,-10}|{1,-20}|", item.NameOfPlane, item.GetConsumptionOfFuel());
+            }
+        }
 
-            //Console.WriteLine(TotalCapacity);
-            //Console.WriteLine(testAiport.GetTotatlCapacity());
-            //Console.WriteLine(testAiport.GetTotalCarryingCapacity());
-            //foreach (var item in testAiport.FindAircraftByConsumptionOfFire(60, 230))
-            //{
-            //    Console.WriteLine(item.Type);
-            //}
-            //testAiport.SortByFlyingRangeDes();
-            //testAiport.SortByFlyingRangeAsc();
+        public static void ShowGetOrderBy(Airport airport)
+        {
+            Console.WriteLine("Демонстрация сортировки по возрастанию");
+            Console.WriteLine("|{0,-10}|{1,-20}|", "Название", "Длина полета");
+            foreach (var item in airport.GetSortByFlyingRangeAsc())
+            {
+                Console.WriteLine("|{0,-10}|{1,-20}|", item.NameOfPlane, item.AircraftProperties_.RangeOfFlight);
+            }
+            Console.WriteLine("\nДемонстрация сортировки по убывания");
+            Console.WriteLine("|{0,-10}|{1,-20}|", "Название", "Длина полета");
+            foreach (var item in airport.GetSortByFlyingRangeDes())
+            {
+                Console.WriteLine("|{0,-10}|{1,-20}|", item.NameOfPlane, item.AircraftProperties_.RangeOfFlight);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Airport airport = new Airport(GetListAircraft());
+
+            Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------\n");
+            ShowAllAircraft(airport);
+            Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------\n");
+            ShowGetAllMethods(airport);
+            Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------\n");
+            ShowGetFindByConmsumptionOfFuel(airport);
+            Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------\n");
+            ShowGetOrderBy(airport);
+            Console.WriteLine("\n-----------------------------------------------------------------------------------------------------------\n");
+
+
         }
     }
 }
